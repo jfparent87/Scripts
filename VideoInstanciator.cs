418adc29d87eacc2c258ded6d4aff67c8e9192ed
GEL\jfpar59 <gausshiggs@gmail.com>
2017-01-19 16:42:21 -0500

    added ar scripts

--- VideoInstanciator.cs
+++ VideoInstanciator.cs
@@ -1,4 +1,4 @@
-using UnityEngine;
+﻿using UnityEngine;
 using System.Collections;
 
 public class VideoInstanciator : MonoBehaviour {
@@ -6,14 +6,27 @@ public class VideoInstanciator : MonoBehaviour {
     public bool isCreated = false;
     public GameObject video;
     public GameObject instantiatedObject;
+    Vector3 position;
+    Quaternion rotation;
+
+    void Start()
+    {
+        position = this.transform.position;
+        position.z = position.z + 0.2f;
+        position.y = position.y + 0.35f;
+        rotation = this.gameObject.transform.rotation;
+        rotation.w -= 1.0f;
+        rotation.y -= 0.7f;
+        rotation.z += 0.7f;
+    }
 
     // Called by GazeGestureManager when the user performs a Select gesture
     void OnSelect()
     {
-        if(!isCreated)
+        if (!isCreated)
         {
-            instantiatedObject = (GameObject)Instantiate(video, new Vector3(0, 0, 3), Quaternion.Euler(-270,180,0));
-            instantiatedObject.transform.parent = GameObject.Find("Room").transform;
+            instantiatedObject = (GameObject)Instantiate(video, position, rotation);
+            instantiatedObject.transform.parent = gameObject.transform.parent;
             isCreated = true;
         }
         else
